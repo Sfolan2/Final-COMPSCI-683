e3bc2194b256b98ba2e903ced070f8238dc2d1d6
Sean Folan <75865821+Sfolan2@users.noreply.github.com>
2024-05-09 22:46:44 -0400

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from first_ai import FishPlayer
 from MCTSPlayer import MCTSPokerPlayer
 from raise_player import RaisedPlayer
 from RandomPlayer import RandomPlayer
-from texasHoldEmState import RLPLayer
+from Group4Player import Group4Player
 
 #TODO:config the config as our wish
 config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
@@ -12,7 +12,7 @@ config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
 
 
 config.register_player(name="f1", algorithm=RandomPlayer())
-config.register_player(name="FT2", algorithm=RLPLayer())
+config.register_player(name="FT2", algorithm=Group4Player())
 
 
 game_result = start_poker(config, verbose=1)
